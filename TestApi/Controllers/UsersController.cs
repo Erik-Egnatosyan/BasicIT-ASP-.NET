0ebc99dc7f8743a5f0f659417e2f57ba781ac0ac
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-04-19 18:41:11 +0400

    Add project files.

---
+++ TestApi/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestApi.Models;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("222222")]
+        public int GetAll()
+        {
+            return 111;
+        }
+        [HttpGet("333333")]
+        public int GetAll2()
+        {
+            return 222;
+        }
+
+
+
+
+
+        [HttpPost("PostAllData")]
+        public ResponseModel PostAll(TestModel testModel)//testmodel @ndunox parametreri model
+        //ResponsModel veradardzvox parametreri model
+        {
+            ResponseModel responseModel = new ResponseModel();
+            responseModel.Id = testModel.Name1 + 100;
+            responseModel.Name = testModel.Name2 + "testing";
+            responseModel.Description = testModel.Name2 + "testing description";
+            return responseModel;
+        }
+    }
+}
---
+++ TestApi/Models/DeliteOneData.cs
@@ -0,0 +1,7 @@
+﻿namespace TestApi.Models
+{
+    public class DeliteOneData
+    {
+        public int Id { get; set; }
+    }
+}
